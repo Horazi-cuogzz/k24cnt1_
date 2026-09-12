40c7b30f74d2552630ba50f588ebf5802eac1411
PHUNG QUANG CUONG <cuonghg333@gmail.com>
2026-09-12 14:49:19 +0700

    update

---
+++ PQCLession06/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PQCLession06.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
